3c5418551137d31edde868906b00c0d86f569d3a
Pranay <ee26btech11230@iith.ac.in>
2026-09-10 16:25:54 +0530

    push

--- 10-09-26/main.py
+++ 10-09-26/main.py
@@ -5,11 +5,16 @@ from math import e
 import matplotlib.pyplot as p
 import numpy as n
 
+#defining x values
 x = n.linspace(-10,10,1000)
 
+#labeling axes
 p.xlabel("X-Axis")
 p.ylabel("Y-Axis")
 
+#plotting graph
 p.plot(x,e**x -2)
+
+#saving
 p.savefig("graph.png")
 
