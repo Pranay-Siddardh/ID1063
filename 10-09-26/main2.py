60ad6725e0802ed8a56e758f00a0d5c087a02342
Pranay <ee26btech11230@iith.ac.in>
2026-09-17 16:48:10 +0530

    Push

--- 10-09-26/main2.py
+++ 10-09-26/main2.py
@@ -1,14 +1,15 @@
+#Code BY T.Pranay
+#Date 10-09-26
+
+import subprocess
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("Equations:")
 print("2x + 3y = 6")
-print("4x + 6y = 3k")
-print()
+print("4x + 6y = 3k \n")
 
-# -------------------------------------------------
-# Function to construct and row-reduce the matrix
-# -------------------------------------------------
-
+#making a solving function for the spec matrix of ours
 def solve_matrix(k):
 
     # Augmented matrix
@@ -50,9 +51,7 @@ def solve_matrix(k):
         print("The two equations represent PARALLEL DISTINCT LINES.")
 
 
-# -------------------------------------------------
 # Same-line case
-# -------------------------------------------------
 
 print("=" * 50)
 print("SAME LINE CASE")
@@ -61,9 +60,7 @@ print("=" * 50)
 solve_matrix(4)
 
 
-# -------------------------------------------------
 # Parallel-line cases
-# -------------------------------------------------
 
 print("\n\n" + "=" * 50)
 print("PARALLEL LINE CASES")
@@ -74,3 +71,19 @@ for k in [1, 2, 3, 5, 6]:
     print("\n" + "-" * 40)
 
     solve_matrix(k)
+
+x = np.linspace(-10,10,1000)
+
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+
+for i in range(1,6):
+    k_val = i
+    
+    y1 = (6-2*x)/3
+    y2 = (3*k_val-4*x)/6
+    plt.plot(x,y1)
+    plt.plot(x,y2)
+
+plt.savefig("graphy.pdf")
+subprocess.run(["termux-open","graphy.pdf"])
