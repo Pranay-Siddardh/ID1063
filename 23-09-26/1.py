a2986ccc088d6d89532401ef6aa73cd796cefa9f
Pranay <ee26btech11230@iith.ac.in>
2026-09-23 15:12:37 +0530

    push

--- 23-09-26/1.py
+++ 23-09-26/1.py
@@ -4,9 +4,9 @@ data = np.array([3,4,0,5])
 data1 = np.array([1,-1,1,-1])
 data2 = np.array([7.5])
 
-rms = np.sqrt(np.mean(data**2))
-rms1 = np.sqrt(np.mean(data1**2))
-rms2 = np.sqrt(np.mean(data2**2))
+rms = np.sqrt(np.mean(np.square(data)))
+rms1 = np.sqrt(np.mean(np.square(data1)))
+rms2 = np.sqrt(np.mean(np.square(data2)))
 
 print(f"The Rms value of the test case 4, 3 4 0 5 is {rms}")
 print(f"The Rms value of the test case 4, 1 -1 1 -1 is {rms1}")
